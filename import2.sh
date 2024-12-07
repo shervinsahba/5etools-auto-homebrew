c64dfdf2580b61e36fbbfdceef9d932544514f27
Shervin Sahba <shervin@tuta.io>
2024-12-07 02:24:10 -0800

    fixed

--- import2.sh
+++ import2.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
 homebrew_dir="/var/www/localhost/htdocs/homebrew"
-homebrew_dir="$(pwd)"
 
-# Initialize an empty list as a space-separated string
 HB_IMPORTS=""
 
 # Process each line in the homebrew_urls file
@@ -11,31 +9,19 @@ while IFS= read -r line; do
     # Split the line by spaces into positional parameters
     set -- $line
     
-    # Download the file with wget
-	rm "$homebrew_dir"/"$1"
-    wget --directory-prefix "$homebrew_dir" "$2"
-    
-    # Get the filename from the URL
-    filename=$(basename "$2")
-    
-    # Replace '%20' with a space in the filename
-    filename=$(echo "$filename" | sed 's/%20/ /g')
-    
-    # Move the file to the desired name
-    mv "$filename" "$1"
-    
-    # Append the new file name to the HB_IMPORTS string
+    wget --directory-prefix "$homebrew_dir" "$1"
+	
+	filename=$(basename "$1" | sed 's/%20/ /g')
+
     if [ -n "$HB_IMPORTS" ]; then
-        HB_IMPORTS="$HB_IMPORTS,$1"
+        HB_IMPORTS="$HB_IMPORTS,\"$filename\""
     else
-        HB_IMPORTS="$1"
+        HB_IMPORTS=\""$filename"\"
     fi
 done < homebrew_urls
 
-# Prepare the final JSON update
-# Remove trailing comma, if any
 HB_IMPORTS="\"$HB_IMPORTS\""
 
 # Update the index.json file with the new import list
-sed -i "s/\"toImport\".*/\"toImport\": [$HB_IMPORTS]/" "$homebrew_dir"/index.json
+sed -i "s/\"toImport\".*/\"toImport\":\ [$HB_IMPORTS]/" "$homebrew_dir"/index.json
 
