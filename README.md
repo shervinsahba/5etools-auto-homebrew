875d83df6f1f33c1b389c761b97a17c5e2cbfead
Shervin Sahba <shervin@tuta.io>
2024-12-07 00:57:01 -0800

    added a /bin/sh compatible import script

--- README.md
+++
@@ -1,4 +0,0 @@
-# Instructions
-- Add desired homebrew to `homebrew_urls` in the format `<desired_filename.json> <url>`. The desired filename is just to save it with a clean name, but it's necessary in the script as is. Each homebrew name and source should be on a newline.
-- Edit the `homebrew_dir` variable if needed.
-- Clone the git repo to your instance of 5etools and run `import.sh`.
---
+++ import2.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+homebrew_dir="/var/www/localhost/htdocs/homebrew"
+homebrew_dir="$(pwd)"
+
+# Initialize an empty list as a space-separated string
+HB_IMPORTS=""
+
+# Process each line in the homebrew_urls file
+while IFS= read -r line; do
+    # Split the line by spaces into positional parameters
+    set -- $line
+    
+    # Download the file with wget
+    wget -N --directory-prefix "$homebrew_dir" "$2"
+    
+    # Get the filename from the URL
+    filename=$(basename "$2")
+    
+    # Replace '%20' with a space in the filename
+    filename=$(echo "$filename" | sed 's/%20/ /g')
+    
+    # Move the file to the desired name
+    mv "$filename" "$1"
+    
+    # Append the new file name to the HB_IMPORTS string
+    if [ -n "$HB_IMPORTS" ]; then
+        HB_IMPORTS="$HB_IMPORTS,$1"
+    else
+        HB_IMPORTS="$1"
+    fi
+done < homebrew_urls
+
+# Prepare the final JSON update
+# Remove trailing comma, if any
+HB_IMPORTS="\"$HB_IMPORTS\""
+
+# Update the index.json file with the new import list
+sed -i "s/\"toImport\".*/\"toImport\": [$HB_IMPORTS]/" "$homebrew_dir"/index.json
+
